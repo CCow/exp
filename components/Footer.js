5bebb4c5aa641e1791949894ae6a056fe616631b
Mitchell Melby <59625575+melbymm@users.noreply.github.com>
2022-05-19 15:05:45 -0700

    Add files via upload

---
+++ components/Footer.js
@@ -0,0 +1,31 @@
+import useItems from "../context/ItemContext";
+import styled from "styled-components";
+
+export default function Footer() {
+    const { items } = useItems()
+
+    return (
+        <Wrapper>
+            <h3>Our Products</h3>
+            <ProductList>
+                {
+                    items?.map((i) =>
+                        <li key={i.name}>{i.name}</li>
+                    )
+                }
+            </ProductList>
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.footer`
+    background-color: #f0f0f0;
+    padding: 20px 10%;
+    padding-bottom: 100px;
+`
+
+const ProductList = styled.ol`
+    list-style: none;
+    margin: 0;
+    padding: 0;
+`
