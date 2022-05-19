20029f3435eed9f417cf90cda4f451124132eb65
Mitchell Melby <59625575+melbymm@users.noreply.github.com>
2022-05-18 18:24:36 -0700

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+import Head from 'next/head'
+import ExperienceList from "../components/ExperienceList"
+export default function Home() {
+  return <>
+    <Head>
+      <title> Exp app </title>
+
+    </Head>
+    <GratitudeApp />
+
+  </>
+}
